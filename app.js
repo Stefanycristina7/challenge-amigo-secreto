0906b437ba95f034d7269ed946002ccabd8cc372
Stéfany Cristina <stefanycristinas009@gmail.com>
2025-03-18 17:27:07 -0300

    Ajustes na função adicionaAmigo

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos= [];
-inserirNomeDosAmigos()
-function inserirNomeDosAmigos(){
-  let nomeDeAmigos = document.getElementById('amigo');
 
+/* Essa finção vai fazer com que os amigos sejam adicionados na lista*/ 
+
+function adicionarAmigo(){
+  let campoAmigo = document.getElementById('amigo');
+  let amigoInserido = campoAmigo.value.trim();
+
+  if(amigoInserido === ''){
+    alert('Por favor, digite um nome válido');
+    return;
+  }
+
+  amigos.push(amigoInserido);
+  campoAmigo.value = ''; // limpa o campo da entrada após nome adicionado
+  campoAmigo.focus();
+  atualizarListaDeAmigos();
 }
